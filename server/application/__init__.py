bdd472f949374b6021a9deab4e4e9276040d5194
nithis-coder-08 <85441522+nithis-coder-08@users.noreply.github.com>
2024-03-18 17:32:25 +0530

    Update __init__.py

--- server/application/__init__.py
+++ server/application/__init__.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 CORS(app)
 try:
     app.config["MONGO_URI"] = "mongodb+srv://nithisthirumalai:MmKUlJgasoz1frPP@cluster0.m0jlvgi.mongodb.net/db?retryWrites=true&w=majority&appName=Cluster0"
-    # app.config["MONGO_URI"] =  "mongodb+srv://harishij101:nFuIx52WonhbVNHy@cluster0.e59kdcr.mongodb.net/DB?retryWrites=true&w=majority&appName=Cluster0"
+   
 except:
-    print('err')
+    print('error occured')
 mongodb_client = PyMongo(app)
 db = mongodb_client.db
 print(mongodb_client)
